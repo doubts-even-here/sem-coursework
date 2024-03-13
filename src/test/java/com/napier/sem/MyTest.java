60a47c693b8e68012ac9b449569af4578961984d
AbbieAirlie <40588538@live.napier.ac.uk>
2024-03-13 16:50:45 +0000

    implementing unit tests - integrating with github

---
+++ src/test/java/com/napier/sem/MyTest.java
@@ -0,0 +1,14 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyTest
+{
+
+}
